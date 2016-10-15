d4cc180504d3e5d37cdf938241bc6c3f499bd093
John Gosset <john@gossetx.com>
2016-10-15 18:14:38 -0400

    Remove barstringer.go (just use fmt.Stringer)

--- barstringer.go
+++
@@ -1,8 +0,0 @@
-package main
-
-// Barstringers should return a string describing the current state of the
-// data, or otherwise if no data is available, the empty string.
-type BarStringer interface {
-	Str() string
-	Update() error
-}
