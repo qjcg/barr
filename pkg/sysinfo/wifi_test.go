a6de41ca4b66f332948c406be55142be222e8aee
John Gosset <john@gossetx.com>
2018-10-30 10:01:39 -0400

    Add wifi test

---
+++ pkg/sysinfo/wifi_test.go
@@ -0,0 +1,13 @@
+package barr
+
+import "testing"
+
+func TestGetConnection(t *testing.T) {
+	w := WifiData{}
+	err := w.getConnection()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Ifname: %s\n", w.Ifname)
+	t.Logf("ESSID: %s\n", w.ESSID)
+}
