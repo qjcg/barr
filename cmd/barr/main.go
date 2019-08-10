3c041b773303bc7ddc9973af7d5f037a59782b22
John Gosset <john@gossetx.com>
2019-08-09 23:04:16 -0400

    Remove cruft

--- cmd/barr/main.go
+++ cmd/barr/main.go
@@ -1,32 +1,20 @@
-// The barr command prints out a status line for use with minimalistic window managers.
+// The barr command prints out a system status line.
 package main
 
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
-	"os/exec"
-	"os/signal"
 	"strings"
-	"time"
 
 	"github.com/qjcg/barr/pkg/sysinfo"
 )
-
-// StatusBar describes a statusbar.
-type StatusBar struct {
-	Stringers []fmt.Stringer
-}
 
 var separator string
 
 func main() {
 	flag.StringVar(&separator, "s", "  ", "output field separator")
-
 	version := flag.Bool("v", false, "print version")
-	xSetRootMode := flag.Bool("x", false, "xsetroot mode (loop and update)")
-	xSetRootModeFreq := flag.Duration("xf", time.Second*5, "xsetroot mode update frequency")
 	flag.Parse()
 
 	if *version {
@@ -47,36 +35,12 @@ func main() {
 		},
 	}
 
-	if *xSetRootMode {
+	fmt.Println(sb.Get())
+}
 
-		// Clear screen and exit when interrupt signal received.
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt, os.Kill)
-		go func(c chan os.Signal) {
-			for range c {
-				exec.Command("/usr/bin/xsetroot", "-name", "barr stopped").Run()
-				os.Exit(0)
-			}
-		}(c)
-
-		// Print once right away.
-		err := sb.UpdateXRootWindowTitle()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// After printing once above, loop and update.
-		// See https://github.com/golang/go/issues/17601
-		ticker := time.NewTicker(*xSetRootModeFreq)
-		for range ticker.C {
-			err := sb.UpdateXRootWindowTitle()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	} else {
-		fmt.Println(sb.Get())
-	}
+// StatusBar describes a statusbar.
+type StatusBar struct {
+	Stringers []fmt.Stringer
 }
 
 // Get returns a status string.
@@ -96,14 +60,4 @@ func (sb *StatusBar) Get() string {
 	output = strings.Trim(output, " ")
 
 	return output
-}
-
-// UpdateXRootWindowTitle sets the X root window title.
-// In dwm, this is also the way to set the WM status string.
-func (sb *StatusBar) UpdateXRootWindowTitle() error {
-	err := exec.Command("/usr/bin/xsetroot", "-name", sb.Get()).Run()
-	if err != nil {
-		return err
-	}
-	return nil
 }
