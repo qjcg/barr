0ec018377ba13bff0e144f7a4b0c2387ebea51a8
John Gosset <john@gossetx.com>
2020-05-23 23:13:34 -0400

    Add a swaybar protocol package

---
+++ pkg/swaybar_protocol/swaybar_protocol.go
@@ -0,0 +1,55 @@
+// See swaybar-protocol(7).
+package swaybar
+
+// Header represents a swaybar-protocol header.
+type Header struct {
+	Version     int
+	ClickEvents bool
+	ContSignal  int
+	StopSignal  int
+}
+
+// Body represents a swaybar-protocol body.
+type Body struct {
+	StatusLines []StatusLine
+}
+
+// StatusLine is a slice of Blocks representing a complete swaybar statusline.
+type StatusLine struct {
+	Blocks []Block
+}
+
+// Block represents a single item in a StatusLine.
+type Block struct {
+	FullText            string
+	ShortText           string
+	Color               string
+	Background          string
+	Border              string
+	BorderTop           int
+	BorderBottom        int
+	BorderLeft          int
+	BorderRight         int
+	MinWidth            int
+	Align               string
+	Name                string
+	Instance            string
+	Urgent              bool
+	Separator           bool
+	SeparatorBlockWidth int
+	Markup              string
+}
+
+// ClickEvent represents a swaybar-protocol click event.
+type ClickEvent struct {
+	Name      string
+	Instance  string
+	X         int
+	Y         int
+	Button    int
+	Event     int
+	RelativeX int
+	RelativeY int
+	Width     int
+	Height    int
+}
